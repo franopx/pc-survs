cd3496fae415323eaad31e1fd63e59921f32d52d
fishman <>
2024-05-17 09:23:46 -0400

    Update connection.gd

--- debug/connection.gd
+++ debug/connection.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const PORT = 6000
+
 var peer = ENetMultiplayerPeer.new()
 @export var player_scene: PackedScene
 
@@ -11,4 +13,17 @@ func _unhandled_input(event):
 			join_server()
 
 func host_server():
-	pass
+	peer.create_server(PORT)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(add_player)
+	add_player()
+	
+func add_player(id = 1):
+	var player = player_scene.instantiate()
+	player.name = str(id)
+	call_deferred("add_child", player)
+	player.position = Vector2(160, 32)
+
+func join_server():
+	peer.create_client("localhost", PORT)
+	multiplayer.multiplayer_peer = peer
